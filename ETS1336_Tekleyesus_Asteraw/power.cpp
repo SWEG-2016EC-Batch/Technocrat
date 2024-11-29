b0961488bbf27058fab7d9bb1f88565da52fb728
Tekleyesus Asteraw <tekleyesus.asteraw@aastustudent.edu.et>
2024-11-29 20:10:59 +0300

    Update power.cpp

--- ETS1336_Tekleyesus_Asteraw/power.cpp
+++ ETS1336_Tekleyesus_Asteraw/power.cpp
@@ -1,1 +1,15 @@
-
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+double number1;
+double number2;
+double result;
+cout<<"enter the base number";
+cin>>number1;
+cout<<"enter the exponent";
+cin>>number2;
+result = pow(number1 , number2);
+cout<<number1<< " the power of "<<number2<< " is "<<result;
+ return 0;
+}
