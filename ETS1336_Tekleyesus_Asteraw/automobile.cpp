bf6770bc717d6932f388fab4d51c54a03a4755b2
Tekleyesus Asteraw <tekleyesus.asteraw@aastustudent.edu.et>
2024-11-29 22:36:40 +0300

    Update automobile.cpp

--- ETS1336_Tekleyesus_Asteraw/automobile.cpp
+++ ETS1336_Tekleyesus_Asteraw/automobile.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+  double gallons;
+  double miles_per_gallons;
+  double miles_left;
+  
+  cout << "Enter the gallons in the aoutomobile: ";
+  cin>>gallons;
+  cout << "Enter the miles_per_gallons of the automobile: ";
+  cin >> miles_per_gallons;
+
+  miles_left = gallons * miles_per_gallons;
+
+  cout << "Miles that are driven without refuelling is: " << miles_left<<" miles" <<  endl; 
+  return 0;
+}
