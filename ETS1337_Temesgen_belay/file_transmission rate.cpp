52971bf041a0515c1c869b7079dd0fa945c44e58
Temesgen belay <Temesgenbelaygirma@gmail.com>
2024-11-30 00:24:23 +0100

    Update file_transmission rate.cpp

--- ETS1337_Temesgen_belay/file_transmission rate.cpp
+++ ETS1337_Temesgen_belay/file_transmission rate.cpp
@@ -1,3 +1,7 @@
+/*A serial transmission line can transmit 960 characters a second. Write a program that will calculate how long
+it will take to send a file, given the file size. Test your program on a 400MB (419,430,400 byte) file which may
+take days.*/
+
 #include <iostream>
 using namespace std;
 
