19b8045e419af9d1426ce73bc4287d6c987c69d8
yared kassu <yared.kassu@aastustudent.edu.et>
2024-12-01 15:20:15 +0300

    transmision.cpp

--- ETS_1414_yared_kassu/transmision.cpp
+++ ETS_1414_yared_kassu/transmision.cpp
@@ -4,9 +4,11 @@ int main(){
     //daclaration
  double days,hours,minutes,seconds;
  //given the file size from the question
- int file_size=419430400;
+ int file_size=;
  int time;
  int transmission_rate=960;
+ cout<< "input the file size";
+ cin>>file_size;
  time=file_size/transmission_rate;
  days=time/86,400;
  time=time%86,400;
