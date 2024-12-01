1bec6236dfe02f6de2e65beddd312ca7f4e01312
Tekleyesus Asteraw <tekleyesus.asteraw@aastustudent.edu.et>
2024-12-01 21:16:22 +0300

    Update file send.cpp

--- ETS1336_Tekleyesus_Asteraw/file send.cpp
+++ ETS1336_Tekleyesus_Asteraw/file send.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main() {
 const int transmission_rate = 960;
-const long long int file_size = 419430400;// 400 MB = 419430400 Bytes
-
+long long int file_size;
+cout<<"enter file size ";
+cin>>file_size;
 long long int time_in_seconds = file_size / transmission_rate;
-
 cout << "The time that is elapsed to transmit "<<file_size<< " Bytes is "<<time_in_seconds << " seconds." << endl;
 return 0;
 }
