b97b28352f4612ea9e28d9784d66208e3f9e89f0
Tekleyesus Asteraw <tekleyesus.asteraw@aastustudent.edu.et>
2024-12-12 14:03:05 -0500

    Update menu.cpp

--- ETS1336_Tekleyesus_Asteraw/menu.cpp
+++ ETS1336_Tekleyesus_Asteraw/menu.cpp
@@ -6,15 +6,12 @@ using namespace std;
 int count_digits();
 int even_multiplication();
 int frequency_digit();
-int grade_calcu();
-int net_pay();
 int palindrome();
 int perfect_num();
 int reverse_number();
 int strong_num();
 int sum_of_first_last();
 int swap_digits();
-
 int main() {
     int choice;
     while (true) {
@@ -22,14 +19,12 @@ int main() {
         cout << "1. Count digits\n";
         cout << "2. Even digits multiplication\n";
         cout << "3. Digit frequency computation\n";
-        cout << "4. Calculate grade\n";
-        cout << "5. Net pay of an employee\n";
-        cout << "6. Palindrome check\n";
-      cout << "7. Perfect number check\n";
-     cout << "8. Reverse digits\n";
-       cout << "9. Strong number check\n";
-        cout << "A. Sum of the first and last digit\n";
-       cout << "B. Swap the first and last digit\n";
+        cout << "4. Palindrome check\n";
+      cout << "5. Perfect number check\n";
+     cout << "6. Reverse digits\n";
+       cout << "7. Strong number check\n";
+        cout << "8. Sum of the first and last digit\n";
+       cout << "9. Swap the first and last digit\n";
        cout << "0. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
@@ -45,27 +40,21 @@ int main() {
                 frequency_digit();
                 break;
             case 4:
-                grade_calcu();
-                break;
-            case 5:
-                net_pay();
-                break;
-            case 6:
                 palindrome();
                 break;
-            case 7:
+            case 5:
               perfect_num();
                 break;
-            case 8:
+            case 6:
                reverse_number();
                 break;
-            case 9:
+            case 7:
               strong_num();
                break;
-            case 'A':
+            case 8:
                 sum_of_first_last();
                 break;
-           case 'B':
+           case 9:
                swap_digits();
                 break;
             case 0:
@@ -130,76 +119,9 @@ int frequency_digit() {
     return 0;
 }
 
-int grade_calcu() {
-    double test, quiz, project, assignment, final_exam, total;
-    cout << "Enter test score: ";
-    cin >> test;
-    cout << "Enter quiz score: ";
-    cin >> quiz;
-    cout << "Enter project score: ";
-    cin >> project;
-    cout << "Enter assignment score: ";
-    cin >> assignment;
-    cout << "Enter final exam score: ";
-    cin >> final_exam;
 
-    total = test + quiz + project + assignment + final_exam;
-    cout << "Grade: ";
-    if (total >= 90) {
-        cout << "A+\n";
-    } else if (total >= 80) {
-        cout << "A\n";
-    } else if (total >= 75) {
-        cout << "B+\n";
-    } else if (total >= 60) {
-        cout << "B\n";
-    } else if (total >= 55) {
-        cout << "C+\n";
-    } else if (total >= 45) {
-        cout << "C\n";
-    } else if (total >= 30) {
-        cout << "D\n";
-    } else {
-        cout << "F\n";
-    }
-    return 0;
-}
 
-int net_pay() {
-    double basic_salary, hours, bonus_rate = 0, bonus = 0, pension, tax_rate, tax, net_salary, gross_salary;
-    cout << "Enter your basic salary: ";
-    cin >> basic_salary;
-    cout << "Enter the hours worked: ";
-    cin >> hours;
 
-    if (hours > 40) {
-        cout << "Enter the bonus rate: ";
-        cin >> bonus_rate;
-        bonus = (hours - 40) * bonus_rate;
-    }
-
-    gross_salary = basic_salary + bonus;
-    pension = basic_salary * 0.07;
-
-    if (basic_salary <= 200) {
-        tax_rate = 0;
-    } else if (basic_salary <= 600) {
-        tax_rate = 0.1;
-    } else if (basic_salary <= 1200) {
-        tax_rate = 0.15;
-    } else if (basic_salary <= 2000) {
-        tax_rate = 0.2;
-    } else if (basic_salary <= 3500) {
-        tax_rate = 0.25;
-    } else {
-        tax_rate = 0.3;
-    }
-
-    tax = basic_salary * tax_rate;
-    net_salary = gross_salary - pension - tax;
-    cout << "Net salary: " << net_salary << endl;
-    return 0;
-}
 
 int palindrome() {
     int num, reversedNum = 0, originalNum, remainder;
