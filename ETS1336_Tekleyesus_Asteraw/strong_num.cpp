c0e87e6c12dee4ba88e899d8dc01c7ca74a867be
Tekleyesus Asteraw <tekleyesus.asteraw@aastustudent.edu.et>
2024-12-12 15:14:15 +0300

    Create strong_num.cpp

---
+++ ETS1336_Tekleyesus_Asteraw/strong_num.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int factorial(int n) {
+    int fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+int main() {
+    int num, originalNum, sum = 0, digit;
+    cout << "Enter a number: ";
+    cin >> num;
+    originalNum = num;
+    while (num != 0) {
+        digit = num % 10; 
+        sum += factorial(digit);  
+        num /= 10; 
+    }
+    if (sum == originalNum) {
+        cout << originalNum << " is a strong number." << endl;
+    } else {
+        cout << originalNum << " is not a strong number." << endl;
+    }
+
+    return 0;
+}
