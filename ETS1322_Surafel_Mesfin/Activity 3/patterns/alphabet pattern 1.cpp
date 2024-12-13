ae95c359948329ccdcee92b48f60c4dc5fd75290
Surafel Mesfin <surafelmesfin123@gmail.com>
2024-12-13 12:21:40 +0300

    Update patterns

---
+++ ETS1322_Surafel_Mesfin/Activity 3/patterns/alphabet pattern 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char alphabet='A';
+    for(int j=1 ;j<=4; j++){
+    for (int i=1 ; i<=6 ;i++){
+     cout<<alphabet<<" ";
+        alphabet ++;
+    }
+    cout<<endl;
+    }
+    return 0;
+}
+/* 
+A B C D E F
+G H I J K L
+M N O P Q R
+S T U V W X
+*/
