111284d508e70cf5489b7cf6a51ede61942e00b2
Surafel Mesfin <surafelmesfin123@gmail.com>
2024-12-13 09:21:54 +0300

    Update reverse.cpp

--- ETS1322_Surafel_Mesfin/Activity 3/playing with numbers/reverse.cpp
+++ ETS1322_Surafel_Mesfin/Activity 3/playing with numbers/reverse.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int num, reversed = 0;
+    cout << "Enter an integer: ";
+    cin >> num;
+    while (num != 0) {
+        int digit = num % 10;
+        reversed = reversed * 10 + digit; 
+        num /= 10; 
+    }
+
+    cout << "Reversed Number: " << reversed << endl;
+    return 0;
+}
