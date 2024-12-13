0e173b1fc2cbe49ac7cf5ea3ed271ba83e3b0b5c
Temesgen belay <Temesgenbelaygirma@gmail.com>
2024-12-13 10:04:48 +0100

    Update and rename shape 5.cpp to updated shape 5.cpp

---
+++ ETS1337_Temesgen_belay/updated shape 5.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << (char)('A' + j - 1) << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+
+//to allow user to input dimension 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+ cout << "Enter the number of rows: ";
+    cin >> rows;
+  for (int i = 1; i <= rows; i++) {
+       for (int j = 1; j <= i; j++) {
+            cout << (char)('A' + j - 1) << " ";
+        }
+        cout << endl;  
+    }
+
+    return 0;
+}
+
