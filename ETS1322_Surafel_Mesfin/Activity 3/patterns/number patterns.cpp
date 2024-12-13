d0c5b2b7c7299c546ef7db28a5b24ce44f027b9d
Surafel Mesfin <surafelmesfin123@gmail.com>
2024-12-13 11:55:42 +0300

    Update number patterns.cpp

--- ETS1322_Surafel_Mesfin/Activity 3/patterns/number patterns.cpp
+++ ETS1322_Surafel_Mesfin/Activity 3/patterns/number patterns.cpp
@@ -1,1 +1,19 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    int num=5;
+    for(int j=1 ;j<=5; j++){
+    for (int i=1; i<=5 ;i++){
+     cout<<i<<" ";
+    }
+    cout<<endl;
+    }
+    return 0;
+}
+/*
+ 1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+ */
