aa98c5266acab730f740c19d17bfbbe95fd65024
Tekleyesus Asteraw <tekleyesus.asteraw@aastustudent.edu.et>
2024-12-13 02:58:30 -0500

    Create abcde.cpp

---
+++ ETS1336_Tekleyesus_Asteraw/abcde.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int rows;
+    cout << "Enter the number of rows ";
+    cin >> rows;
+
+    for (int i = 0; i < rows; i++) {
+        cout << "abcde" << endl;
+    }
+
+    return 0;
+}
