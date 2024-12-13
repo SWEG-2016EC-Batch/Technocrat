74367f3b28435954417dfb4a68e200bc3f4bc08a
Tekleyesus Asteraw <tekleyesus.asteraw@aastustudent.edu.et>
2024-12-13 01:40:13 -0500

    Update rectangle_alphabet.cpp

--- ETS1336_Tekleyesus_Asteraw/rectangle_alphabet.cpp
+++ ETS1336_Tekleyesus_Asteraw/rectangle_alphabet.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int num;
+    cout<<"enter the dimension of the shapes ";
+    cin>>num;
     char alpha ='A';
-    for(int i =0;i<4;i++){
-        for(int j=1;j<=6;j++){
+    for(int i =0;i<num;i++){
+        for(int j=1;j<=num+2;j++){
             cout<<alpha;
             alpha++;
         }
